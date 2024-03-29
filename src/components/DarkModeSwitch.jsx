ed585b719af89b62835a7c024233c0e82d14cfbf
Luka Abutidze <lukaabutidze99@gmail.com>
2024-03-29 15:59:02 +0400

    added Dark Mode

---
+++ src/components/DarkModeSwitch.jsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { MdLightMode, MdDarkMode } from "react-icons/md";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+const DarkModeSwitch = () => {
+  const { theme, setTheme, systemTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  const currentTheme = theme === "system" ? systemTheme : theme;
+  useEffect(() => setMounted(true), []);
+
+  return (
+    <div>
+      {mounted &&
+        (currentTheme === "dark" ? (
+          <MdLightMode
+            onClick={() => setTheme("light")}
+            className="text-2xl cursor-pointer hover:text-amber-500"
+          />
+        ) : (
+          <MdDarkMode
+            onClick={() => setTheme("dark")}
+            className="text-2xl cursor-pointer hover:text-amber-500"
+          />
+        ))}
+    </div>
+  );
+};
+
+export default DarkModeSwitch;
