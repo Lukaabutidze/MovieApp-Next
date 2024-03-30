91fcfc96978d62bdf0541b989916faf83d01e66e
Luka Abutidze <lukaabutidze99@gmail.com>
2024-03-30 15:25:15 +0400

    fetch data

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,25 @@
-import React from "react";
+import Results from "@/components/Results";
 
-const Home = () => {
-  return <div>Home</div>;
+const API_KEY = process.env.API_KEY;
+
+const Home = async ({ searchParams }) => {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1`
+  );
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const results = data.results;
+
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/components/Results.jsx
@@ -0,0 +1,13 @@
+const Results = ({ results }) => {
+  return (
+    <div>
+      {results.map((result) => (
+        <div key={result.id}>
+          <h2>{result.original_title}</h2>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Results;
