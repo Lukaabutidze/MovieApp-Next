58c78d9832512c79cee74ce8fa26e5f13976604b
Luka Abutidze <lukaabutidze99@gmail.com>
2024-03-30 15:44:25 +0400

    loading effect

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+const loading = () => {
+  return (
+    <div className="flex justify-center mt-16">
+      <img className="h-52" src="spinner.svg" alt="loading" />
+    </div>
+  );
+};
+
+export default loading;
