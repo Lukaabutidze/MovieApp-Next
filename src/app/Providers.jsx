9c23595ecd24cbe90b1027e8ee780ec88c3fefc8
Luka Abutidze <lukaabutidze99@gmail.com>
2024-04-02 18:16:22 +0400

    another commit

--- src/app/Providers.jsx
+++ src/app/Providers.jsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from "next-themes";
 const Providers = ({ children }) => {
   return (
     <ThemeProvider defaultTheme="system" attribute="class">
-      <div className="text-gray-700 dark:text-gray-200 dark:bg-gray-700 min-h-screen select-none transition-colors duration-300">
+      <div className="text-gray-700 dark:text-gray-200 dark:bg-gray-700 min-h-screen select-none transition-colors duration-200">
         {children}
       </div>
     </ThemeProvider>
--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 
 const MoviePage = async ({ params }) => {
   const movieId = params.id;
+
   const res = await fetch(
     `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.API_KEY}`
   );
@@ -10,7 +11,7 @@ const MoviePage = async ({ params }) => {
 
   return (
     <div className="w-full">
-      <div className="p-4 md:pt-8 flex flex-col md:flex-row content-center max-w-6xl mx-auto md:space-x-6">
+      <div className="p-4 md:pt-8 flex flex-col md:flex-row content-center max-w-6xl mx-auto md:space-x-6 shadow-xl mt-5">
         <Image
           src={`https://image.tmdb.org/t/p/original/${
             movie.backdrop_path || movie.poster_path
